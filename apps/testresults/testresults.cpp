1cd017057e86e26439c60da38725d5ffe3d067d2
devel <djsperka@ucdavis.edu>
2013-05-11 00:05:15 +0000

    *** empty log message ***

--- apps/testresults/testresults.cpp
+++ apps/testresults/testresults.cpp
@@ -1,6 +1,52 @@
 #include <QApplication>
 #include <QMessageBox>
 #include "testresults.h"
+#include "experimentsettings.h"
+#include "runsettings.h"
+#include "subjectsettings.h"
+#include "HEventLog.h"
+#include "HResults.h"
+
+#include <QtGui>
+//#include <QTextEdit>
+
+QTextEdit* createSummary(const HResults* results)
+{
+	QTextEdit* editor = new QTextEdit;
+    QTextCursor cursor(editor->textCursor());
+    cursor.movePosition(QTextCursor::Start);
+
+    QTextFrame *topFrame = cursor.currentFrame();
+    QTextFrameFormat topFrameFormat = topFrame->frameFormat();
+    topFrameFormat.setPadding(16);
+    topFrame->setFrameFormat(topFrameFormat);
+
+    QTextCharFormat textFormat;
+    QTextCharFormat boldFormat;
+    boldFormat.setFontWeight(QFont::Bold);
+
+    cursor.setPosition(topFrame->lastPosition());
+    cursor.insertText("Experiment: ", textFormat);
+    cursor.insertText(results->experimentSettings().getName(), boldFormat);
+    cursor.insertBlock();
+    cursor.insertText("Results file: ", textFormat);
+    cursor.insertText(results->originalFilename(), boldFormat);
+    cursor.insertBlock();
+    cursor.insertText("Habit version:", textFormat);
+    cursor.insertText(results->version(), boldFormat);
+    cursor.insertBlock();
+    cursor.insertText("Data collected on: ", textFormat);
+    cursor.insertText("need date", boldFormat);
+    cursor.insertHtml("<hr width=\"100%\"><br>");
+
+    //! [4] //! [5]
+    cursor.insertBlock();
+    cursor.insertBlock();
+
+    editor->setReadOnly(true);
+    return editor;
+}
+
 
 int main(int argc, char **argv)
 {
@@ -9,23 +55,25 @@ int main(int argc, char **argv)
 	app.setOrganizationName("Infant Cognition Lab");
 	app.setOrganizationDomain("infantcognitionlab.ucdavis.edu");
 
-	QString filename = QFileDialog::getOpenFileName(this, tr("Open File"), "~/Desktop", tr("Habit Results Files (*.res)"));
+	QString filename = QFileDialog::getOpenFileName(NULL, "Open File", "~/Desktop", "Habit Results Files (*.hab)");
 	if (!filename.isNull())
 	{
-		Habit::ExperimentSettings expSettings;
-		Habit::SubjectSettings subjectSettings;
-		Habit::RunSettings runSettings;
-		HEventLog log;
-
-		if (loadExperimentResults(filename, expSettings, runSettings, subjectSettings, log))
+		HResults* results = HResults::load(filename);
+		if (!results)
 		{
-			// Got result. Generate summary.
-			QDialog dialog;
-
+			QMessageBox msgBox;
+			msgBox.setText("Cannot save data.");
+			msgBox.exec();
 		}
 		else
 		{
-			// error opening/reading results
+			// Got result. Generate summary.
+			QDialog dialog;
+			QVBoxLayout* layout = new QVBoxLayout;
+			layout->addWidget(createSummary(results));
+			dialog.setLayout(layout);
+			dialog.exec();
+			delete results;
 		}
 	}
 
