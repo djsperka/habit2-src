de98ceee7cdae20a4cebaba03790a392b9b42d8c
devel <djsperka@ucdavis.edu>
2013-05-16 23:44:36 +0000

    work in progress

--- apps/testresults/testresults.cpp
+++ apps/testresults/testresults.cpp
@@ -25,27 +25,193 @@ QTextEdit* createSummary(const HResults* results)
     QTextCharFormat boldFormat;
     boldFormat.setFontWeight(QFont::Bold);
 
-    cursor.setPosition(topFrame->lastPosition());
-    cursor.insertText("Experiment: ", textFormat);
-    cursor.insertText(results->experimentSettings().getName(), boldFormat);
-    cursor.insertBlock();
-    cursor.insertText("Results file: ", textFormat);
-    cursor.insertText(results->originalFilename(), boldFormat);
-    cursor.insertBlock();
-    cursor.insertText("Habit version:", textFormat);
-    cursor.insertText(results->version(), boldFormat);
-    cursor.insertBlock();
-    cursor.insertText("Data collected on: ", textFormat);
-    cursor.insertText("need date", boldFormat);
-    cursor.insertHtml("<hr width=\"100%\"><br>");
+    QTextTableFormat tableFormat;
+    tableFormat.setBackground(QColor("lightGray"));
+    tableFormat.setBorder(0);
+    QVector<QTextLength> constraints;
+    constraints << QTextLength(QTextLength::PercentageLength, 25);
+    constraints << QTextLength(QTextLength::PercentageLength, 75);
+    tableFormat.setColumnWidthConstraints(constraints);
 
-    //! [4] //! [5]
+    cursor.setPosition(topFrame->lastPosition());
+    QTextTable *table = cursor.insertTable(4, 2, tableFormat);
+    //QTextTable *table = cursor.insertTable(4, 2);
+
+    // First row, exp name
+    QTextTableCell cell;
+    cell = table->cellAt(0, 0);
+    cursor = cell.firstCursorPosition();
+    cursor.insertText("Experiment:", textFormat);
+
+    cell = table->cellAt(0, 1);
+    cursor = cell.firstCursorPosition();
+    cursor.insertText(results->experimentSettings().getName(), boldFormat);
+
+    cell = table->cellAt(1, 0);
+    cursor = cell.firstCursorPosition();
+    cursor.insertText("Habit version:", textFormat);
+
+    cell = table->cellAt(1, 1);
+    cursor = cell.firstCursorPosition();
+    cursor.insertText(results->version(), boldFormat);
+
+    cell = table->cellAt(2, 0);
+    cursor = cell.firstCursorPosition();
+    cursor.insertText("Results type:", textFormat);
+
+    cell = table->cellAt(2, 1);
+    cursor = cell.firstCursorPosition();
+    cursor.insertText("Original run", boldFormat);
+
+    cell = table->cellAt(3, 0);
+    cursor = cell.firstCursorPosition();
+    cursor.insertText("Results file:", textFormat);
+
+    cell = table->cellAt(3, 1);
+    cursor = cell.firstCursorPosition();
+    cursor.insertText(results->originalFilename(), boldFormat);
+
+    // move cursor to end, insert new frame
+    cursor = editor->textCursor();
+    cursor.movePosition(QTextCursor::End);
+    cursor.insertFrame(topFrameFormat);
     cursor.insertBlock();
-    cursor.insertBlock();
+
+
+    QTextTableFormat lookTableFormat;
+    lookTableFormat.setBackground(QColor("lightGray"));
+    lookTableFormat.setBorder(0);
+    constraints.clear();
+    constraints << QTextLength(QTextLength::PercentageLength, 20);
+    constraints << QTextLength(QTextLength::PercentageLength, 10);
+    constraints << QTextLength(QTextLength::PercentageLength, 10);
+    constraints << QTextLength(QTextLength::PercentageLength, 20);
+    constraints << QTextLength(QTextLength::PercentageLength, 10);
+    constraints << QTextLength(QTextLength::PercentageLength, 30);
+    lookTableFormat.setColumnWidthConstraints(constraints);
+
+    table = cursor.insertTable(1, 6, lookTableFormat);
+    cell = table->cellAt(0, 0);
+    cursor = cell.firstCursorPosition();
+    cursor.insertText("Phase", boldFormat);
+    cell = table->cellAt(0, 1);
+    cursor = cell.firstCursorPosition();
+    cursor.insertText("Trial", boldFormat);
+    cell = table->cellAt(0, 2);
+    cursor = cell.firstCursorPosition();
+    cursor.insertText("Repeat", boldFormat);
+    cell = table->cellAt(0, 3);
+    cursor = cell.firstCursorPosition();
+    cursor.insertText("Direction", boldFormat);
+    cell = table->cellAt(0, 4);
+    cursor = cell.firstCursorPosition();
+    cursor.insertText("Duration", boldFormat);
+    cell = table->cellAt(0, 5);
+    cursor = cell.firstCursorPosition();
+    cursor.insertText("Notes", boldFormat);
+
+
+    // iterate over event log.
+    // When PhaseStart/PhaseEnd encountered, enter with a Note.
+	QListIterator<HEvent*> events(results->eventLog());
+	events.toFront();
+
+	QString sPhase("");
+	int iTrialNumber = 0;
+	int iRepeatNumber = -1;
+	while (events.hasNext())
+	{
+		HEvent* e = events.next();
+		if (e->type() == HEventType::HEventPhaseStart)
+		{
+			HPhaseStartEvent* ps = static_cast<HPhaseStartEvent*>(e);
+			sPhase = ps->phase();
+			// append row, indicate start of phase
+			table->appendRows(1);
+			cell = table->cellAt(table->rows()-1, 0);
+			cursor = cell.firstCursorPosition();
+			cursor.insertText(sPhase, textFormat);
+			cell = table->cellAt(table->rows()-1, 5);
+			cursor = cell.firstCursorPosition();
+			cursor.insertText("Phase Start", textFormat);
+		}
+		else if (e->type() == HEventType::HEventPhaseEnd)
+		{
+			// append row, indicate end of phase
+			table->appendRows(1);
+			cell = table->cellAt(table->rows()-1, 0);
+			cursor = cell.firstCursorPosition();
+			cursor.insertText(sPhase, textFormat);
+			cell = table->cellAt(table->rows()-1, 5);
+			cursor = cell.firstCursorPosition();
+			cursor.insertText("Phase End", textFormat);
+			sPhase = "";
+		}
+		else if (e->type() == HEventType::HEventTrialStart)
+		{
+			HTrialStartEvent* ps = static_cast<HTrialStartEvent*>(e);
+			iTrialNumber = ps->trialnumber();
+			iRepeatNumber = ps->repeatnumber();
+			table->appendRows(1);
+			cell = table->cellAt(table->rows()-1, 0);
+			cursor = cell.firstCursorPosition();
+			cursor.insertText(sPhase, textFormat);
+			cell = table->cellAt(table->rows()-1, 1);
+			cursor = cell.firstCursorPosition();
+			cursor.insertText(QString("%1").arg(iTrialNumber), textFormat);
+			cell = table->cellAt(table->rows()-1, 2);
+			cursor = cell.firstCursorPosition();
+			cursor.insertText(QString("%1").arg(iRepeatNumber), textFormat);
+			cell = table->cellAt(table->rows()-1, 5);
+			cursor = cell.firstCursorPosition();
+			cursor.insertText("Trial Start", textFormat);
+		}
+		else if (e->type() == HEventType::HEventTrialEnd)
+		{
+			HTrialEndEvent* ps = static_cast<HTrialEndEvent*>(e);
+			table->appendRows(1);
+			cell = table->cellAt(table->rows()-1, 0);
+			cursor = cell.firstCursorPosition();
+			cursor.insertText(sPhase, textFormat);
+			cell = table->cellAt(table->rows()-1, 1);
+			cursor = cell.firstCursorPosition();
+			cursor.insertText(QString("%1").arg(iTrialNumber), textFormat);
+			cell = table->cellAt(table->rows()-1, 2);
+			cursor = cell.firstCursorPosition();
+			cursor.insertText(QString("%1").arg(iRepeatNumber), textFormat);
+			cell = table->cellAt(table->rows()-1, 5);
+			cursor = cell.firstCursorPosition();
+			cursor.insertText(QString("Trial End (%1)").arg(ps->endtype().name()), textFormat);
+		}
+		else if (e->type() == HEventType::HEventLook)
+		{
+			HLookEvent* ps = static_cast<HLookEvent*>(e);
+			table->appendRows(1);
+			cell = table->cellAt(table->rows()-1, 0);
+			cursor = cell.firstCursorPosition();
+			cursor.insertText(sPhase, textFormat);
+			cell = table->cellAt(table->rows()-1, 1);
+			cursor = cell.firstCursorPosition();
+			cursor.insertText(QString("%1").arg(iTrialNumber), textFormat);
+			cell = table->cellAt(table->rows()-1, 2);
+			cursor = cell.firstCursorPosition();
+			cursor.insertText(QString("%1").arg(iRepeatNumber), textFormat);
+			cell = table->cellAt(table->rows()-1, 3);
+			cursor = cell.firstCursorPosition();
+			cursor.insertText("TODO", textFormat);
+			cell = table->cellAt(table->rows()-1, 4);
+			cursor = cell.firstCursorPosition();
+			cursor.insertText(QString("%1").arg(ps->look().lookMS()), textFormat);
+			cell = table->cellAt(table->rows()-1, 5);
+			cursor = cell.firstCursorPosition();
+			cursor.insertText("look", textFormat);
+		}
+	}
 
     editor->setReadOnly(true);
     return editor;
 }
+
 
 
 int main(int argc, char **argv)
@@ -62,7 +228,7 @@ int main(int argc, char **argv)
 		if (!results)
 		{
 			QMessageBox msgBox;
-			msgBox.setText("Cannot save data.");
+			msgBox.setText("Cannot load data.");
 			msgBox.exec();
 		}
 		else
