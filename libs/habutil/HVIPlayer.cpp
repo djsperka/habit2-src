eac4bef98c48c5e2173def79253726d6ebb95b80
devel <djsperka@ucdavis.edu>
2013-12-10 22:47:17 +0000

    Updates, ready for testing.

--- libs/habutil/HVIPlayer.cpp
+++ libs/habutil/HVIPlayer.cpp
@@ -87,35 +87,29 @@ HPlayer(id, w), m_pendingClear(false), m_parent(w), m_pMediaObject(0), m_pVideoW
 HVIPlayer::~HVIPlayer()
 {
 	qDebug("HVIPlayer::~HVIPlayer()");
-	m_pVideoWidget->hide();
-	m_pVideoWidget->close();
-    delete m_pVideoWidget;
-    m_pVideoWidget = 0;
+//    delete m_pVideoWidget;
+//    m_pVideoWidget = 0;
 
-    delete m_pAudioOutput;
-    m_pAudioOutput = 0;
+//    delete m_pAudioOutput;
+//    m_pAudioOutput = 0;
 
-    delete m_pMediaObject;
-    m_pMediaObject = 0;
+//    delete m_pMediaObject;
+//    m_pMediaObject = 0;
 }
 
 
 void HVIPlayer::onStateChanged(Phonon::State newState, Phonon::State oldState)
 {
 	//Q_UNUSED(oldState);
+
+	//QSize s = sizeHint();
+	//qDebug() << (int)oldState << "->" << (int)newState << " " << s.width() << "x" << s.height();
+
 	if (newState == Phonon::PlayingState)
 	{
 		emit started(m_id);
-		QSize s = sizeHint();
-		qDebug() << (int)oldState << "->" << "Playing" << " " << s.width() << "x" << s.height();
 	}
 
-	// test
-	if (oldState == Phonon::LoadingState)
-	{
-		QSize s = sizeHint();
-		qDebug() << "Loading->" << (int)newState << " " << s.width() << "x" << s.height();
-	}
 	return;
 }
 
@@ -136,7 +130,8 @@ void HVIPlayer::onImagePainted()
 
 void HVIPlayer::stop()
 {
-	m_pMediaObject->stop();
+	if (getCurrentStimulusType() == HStimulusSource::VIDEO)
+		m_pMediaObject->stop();
 }
 
 void HVIPlayer::clear()
@@ -158,24 +153,36 @@ void HVIPlayer::clear()
 void HVIPlayer::play(int number)
 {
 	HStimulusSource::HStimulusSourceType newType = getStimulusType(number);
+	HStimulusSource *s;	// Beware! play() can be called with number = -1 (for background). Must not fetch source in that case.
+
 	switch (newType)
 	{
 		case HStimulusSource::VIDEO:
-			m_pMediaObject->setCurrentSource((m_sources.at(number)).filename());
-			//m_pVideoWidget->setGeometry(geometry());
-			qDebug() << "VIDEO player geom " << geometry().left() << "," << geometry().bottom() << "," << geometry().width() << "," << geometry().height();
-			qDebug() << "VIDEO widget geom " << m_pVideoWidget->geometry().left() << "," << m_pVideoWidget->geometry().bottom() << "," << m_pVideoWidget->geometry().width() << "," << m_pVideoWidget->geometry().height();
-			m_pAudioOutput->setVolume(m_sources.at(number).getAudioBalance());
+			s = m_sources.at(number);
+			if (s->hasBuffer())
+			{
+				//m_pMediaObject->setCurrentSource(*s->mediaSource());
+				m_pMediaObject->setCurrentSource(*(new Phonon::MediaSource(s->buffer())));
+			}
+			else
+			{
+				m_pMediaObject->setCurrentSource(s->filename());
+			}
+
+			//qDebug() << "VIDEO player geom " << geometry().left() << "," << geometry().bottom() << "," << geometry().width() << "," << geometry().height();
+			//qDebug() << "VIDEO widget geom " << m_pVideoWidget->geometry().left() << "," << m_pVideoWidget->geometry().bottom() << "," << m_pVideoWidget->geometry().width() << "," << m_pVideoWidget->geometry().height();
+			m_pAudioOutput->setVolume(s->getAudioBalance());
 			m_pMediaObject->play();
 			m_pStackedLayout->setCurrentIndex(1);
 			//m_pVideoWidget->setFullScreen(m_isFullScreen);
 			break;
 		case HStimulusSource::IMAGE:
+			s = m_sources.at(number);
 			m_pImageWidget->setGeometry(QRect(0, 0, geometry().width(), geometry().height()));
-			m_pImageWidget->setCurrentSource((m_sources.at(number)).filename());
+			m_pImageWidget->setCurrentSource(s->filename());
 			m_pStackedLayout->setCurrentIndex(0);
-			qDebug() << "IMAGE player geom " << geometry().left() << "," << geometry().bottom() << "," << geometry().width() << "," << geometry().height();
-			qDebug() << "IMAGE widget geom " << m_pImageWidget->geometry().left() << "," << m_pImageWidget->geometry().bottom() << "," << m_pImageWidget->geometry().width() << "," << m_pImageWidget->geometry().height();
+			//qDebug() << "IMAGE player geom " << geometry().left() << "," << geometry().bottom() << "," << geometry().width() << "," << geometry().height();
+			//qDebug() << "IMAGE widget geom " << m_pImageWidget->geometry().left() << "," << m_pImageWidget->geometry().bottom() << "," << m_pImageWidget->geometry().width() << "," << m_pImageWidget->geometry().height();
 			break;
 		case HStimulusSource::BACKGROUND:
 			m_pStackedLayout->setCurrentIndex(-1);
@@ -188,107 +195,13 @@ void HVIPlayer::play(int number)
 }
 
 
-
-
-
-
-
-
-#if 0
-void HVIPlayer::play(int number)
-{
-	HStimulusSource::HStimulusSourceType newType = getStimulusType(number);
-	switch (getCurrentStimulusType())
-	{
-		case HStimulusSource::BACKGROUND:
-			// Init state, or background state
-			switch (newType) {
-				case HStimulusSource::VIDEO:
-					m_pMediaObject->setCurrentSource((m_sources.at(number)).filename());
-					m_pVideoWidget->setGeometry(geometry());
-					qDebug() << "background-VIDEO geom " << geometry().left() << "," << geometry().bottom() << "," << geometry().width() << "," << geometry().height();
-					qDebug() << "background-VIDEO geom " << m_pVideoWidget->geometry().left() << "," << m_pVideoWidget->geometry().bottom() << "," << m_pVideoWidget->geometry().width() << "," << m_pVideoWidget->geometry().height();
-					m_pAudioOutput->setVolume(m_sources.at(number).getAudioBalance());
-					m_pMediaObject->play();
-					m_pStackedWidget->setCurrentIndex(1);
-					//m_pVideoWidget->setFullScreen(m_isFullScreen);
-					break;
-				case HStimulusSource::IMAGE:
-					m_pImageWidget->setGeometry(QRect(0, 0, geometry().width(), geometry().height()));
-					m_pImageWidget->setCurrentSource((m_sources.at(number)).filename());
-					m_pStackedWidget->setCurrentIndex(0);
-					break;
-				default:
-					break;
-			}
-			break;
-		case HStimulusSource::VIDEO:
-			// showing video - stop it
-			stop();
-			switch (newType) {
-				case HStimulusSource::BACKGROUND:
-					m_pVideoWidget->hide();
-					break;
-				case HStimulusSource::VIDEO:
-					m_pMediaObject->setCurrentSource((m_sources[number].filename()));
-					m_pVideoWidget->setGeometry(geometry());
-					qDebug() << "VIDEO-VIDEO geom " << geometry().left() << "," << geometry().bottom() << "," << geometry().width() << "," << geometry().height();
-					qDebug() << "VIDEO-VIDEO geom " << m_pVideoWidget->geometry().left() << "," << m_pVideoWidget->geometry().bottom() << "," << m_pVideoWidget->geometry().width() << "," << m_pVideoWidget->geometry().height();
-					m_pAudioOutput->setVolume(m_sources.at(number).getAudioBalance());
-					m_pMediaObject->play();
-					m_pStackedWidget->setCurrentIndex(1);	// no need, really
-					//m_pVideoWidget->setFullScreen(m_isFullScreen);
-					break;
-				case HStimulusSource::IMAGE:
-					m_pImageWidget->setGeometry(QRect(0, 0, geometry().width(), geometry().height()));
-					m_pStackedWidget->setCurrentIndex(0);
-					m_pImageWidget->setCurrentSource((m_sources.at(number)).filename());
-					break;
-				default:
-					break;
-			}
-			break;
-		case HStimulusSource::IMAGE:
-			// showing image
-			switch (newType) {
-				case HStimulusSource::BACKGROUND:
-					m_pStackedWidget->setCurrentIndex(-1);
-					break;
-				case HStimulusSource::VIDEO:
-					m_pMediaObject->setCurrentSource((m_sources[number].filename()));
-					m_pVideoWidget->setGeometry(geometry());
-					qDebug() << "image-VIDEO geom " << geometry().left() << "," << geometry().bottom() << "," << geometry().width() << "," << geometry().height();
-					qDebug() << "image-VIDEO geom " << m_pVideoWidget->geometry().left() << "," << m_pVideoWidget->geometry().bottom() << "," << m_pVideoWidget->geometry().width() << "," << m_pVideoWidget->geometry().height();
-					m_pStackedWidget->setCurrentIndex(1);
-					m_pAudioOutput->setVolume(m_sources.at(number).getAudioBalance());
-					m_pMediaObject->play();
-					//m_pVideoWidget->setFullScreen(m_isFullScreen);
-					if (m_parent) m_parent->activateWindow(); //Hack Alert!
-					break;
-				case HStimulusSource::IMAGE:
-					m_pImageWidget->setCurrentSource((m_sources.at(number)).filename());
-					m_pStackedWidget->setCurrentIndex(0);
-					break;
-				default:
-					break;
-			}
-		default:
-			break;
-	}
-	m_iCurrentStim = number;
-
-}
-#endif
-
 void HVIPlayer::onPrefinishMarkReached(qint32 msec)
 {
 	Q_UNUSED(msec);
-	if (m_iCurrentStim > -1 && m_iCurrentStim < m_sources.count() && (m_sources.at(m_iCurrentStim)).type() == HStimulusSource::VIDEO && (m_sources.at(m_iCurrentStim)).isLooped())
+	if (m_iCurrentStim > -1 && m_iCurrentStim < m_sources.count() && (m_sources.at(m_iCurrentStim))->type() == HStimulusSource::VIDEO && (m_sources.at(m_iCurrentStim))->isLooped())
 	{
 		m_pMediaObject->pause();
 		m_pMediaObject->seek(0);
 		m_pMediaObject->play();
 	}
 }
-
-
