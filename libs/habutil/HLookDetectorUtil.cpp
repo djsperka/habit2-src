90018e9e9dcdbd9417ef27fe247d9f0f5c2b4fd5
devel <djsperka@ucdavis.edu>
2014-01-29 01:58:59 +0000

    Fix compile errors and add to regular build.

--- libs/habutil/HLookDetectorUtil.cpp
+++ libs/habutil/HLookDetectorUtil.cpp
@@ -6,6 +6,7 @@
  */
 
 #include "HLookDetectorUtil.h"
+#include "HKeypadLookDetector.h"
 
 
 HLookDetector* createLookDetector(const Habit::ExperimentSettings& es, HEventLog& log, QWidget* pdialog)
@@ -13,9 +14,16 @@ HLookDetector* createLookDetector(const Habit::ExperimentSettings& es, HEventLog
 	HLookDetector *pld = (HLookDetector *)NULL;
 	int lookTimeMS = es.getDesignSettings().getPretestTrialsInfo().getLookTimes();
 	int lookAwayTimeMS = es.getDesignSettings().getHabituationTrialsInfo().getLookTimes();
-	pld = new HKeypadLookDetector(lookTimeMS, lookAwayTimeMS, log, this);
+
+	// Assume that we use the keypad look detector.
+	// This will change when habit is modified to allow using eye trackers. Presumably the experiment
+	// settings, or maybe the RunSettings will have an indication of the look detector type
+	// to be used.
+
+	pld = new HKeypadLookDetector(lookTimeMS, lookAwayTimeMS, log, pdialog);
 	return pld;
 }
 
+//m_pld = new HKeypadLookDetector(minLook, minLookAway, m_eventLog, this, true, false, true);
 
 
