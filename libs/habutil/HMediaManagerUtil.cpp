45c75318ca764560d1f827ca87bddc36e371270c
djsperka <djsperka@ucdavis.edu>
2018-04-23 05:38:43 +0000

    Remove unused code, fix includes in HMediaManagerUtil.

--- libs/habutil/HMediaManagerUtil.cpp
+++ libs/habutil/HMediaManagerUtil.cpp
@@ -7,25 +7,14 @@
  *
  */
 
-#include <hgst/HStimulusWidget.h>
-#include "HMediaManagerUtil.h"
+#include "HGMM.h"
+#include "HStimulusWidget.h"
 #include "HStimPipeline.h"
 #include <QtGlobal>
 
-#if QT_VERSION >= 0x050000
 #include <QDesktopWidget>
 #include <QApplication>
-#else
-#include "HVIPlayer.h"
-#include "HAudioPlayer.h"
-#endif
 #include "HWorkspaceUtil.h"
-
-
-#if QT_VERSION >= 0x050000
-
-
-// Always first create a media manager.
 
 HGMM* createMediaManager(const Habit::ExperimentSettings& es)
 {
@@ -72,10 +61,6 @@ HGMM* createMediaManager(const Habit::ExperimentSettings& es)
 }
 
 
-HGMM* createMediaManager(const Habit::ExperimentSettings& es, int screenWidth, int screenHeight)
-{
-	return createMediaManager(es.getStimulusDisplayInfo(), screenWidth, screenHeight);
-}
 
 HGMM* createMediaManager(const Habit::StimulusDisplayInfo& sdi, int screenWidth, int screenHeight)
 {
@@ -107,68 +92,7 @@ HGMM* createMediaManager(const Habit::StimulusDisplayInfo& sdi, int screenWidth,
 	return pmm;
 }
 
-#else
-
-HGMM* createMediaManager(const Habit::ExperimentSettings& es)
+HGMM* createMediaManager(const Habit::ExperimentSettings& es, int screenWidth, int screenHeight)
 {
-	HGMM* pmm = new HGMM();
-
-	// Stimulus Display info
-	Habit::StimulusDisplayInfo sdi = es.getStimulusDisplayInfo();
-	// Create players for each monitor that will be used. 
-	int iControl = habutilGetMonitorID(HPlayerPositionType::Control);
-	int iLeft = habutilGetMonitorID(HPlayerPositionType::Left);
-	int iCenter = habutilGetMonitorID(HPlayerPositionType::Center);
-	int iRight = habutilGetMonitorID(HPlayerPositionType::Right);
-	const HStimulusLayoutType& layoutType = sdi.getStimulusLayoutType();
-
-	QDir baseDir;
-	habutilGetStimulusRootDir(baseDir);
-
-
-	if (iControl > -1 && sdi.getUseISS())
-	{
-		HPlayer *audio = new HAudioPlayer(iControl, NULL, baseDir);
-		pmm->addPlayer(HPlayerPositionType::Sound, audio, iControl);
-	}
-	if (layoutType == HStimulusLayoutType::HStimulusLayoutSingle)
-	{
-		HPlayer *single = new HVIPlayer(iCenter, NULL, baseDir, sdi.getDisplayType() == HDisplayType::HDisplayTypeFullScreen, sdi.getBackGroundColor(), sdi.isOriginalAspectRatioMaintained());
-		single->setPreferBufferedStimulus(false);
-		pmm->addPlayer(HPlayerPositionType::Center, single, iCenter);
-	}
-	else if (layoutType == HStimulusLayoutType::HStimulusLayoutLeftRight)
-	{
-		HPlayer *left = new HVIPlayer(iLeft, NULL, baseDir, sdi.getDisplayType() == HDisplayType::HDisplayTypeFullScreen, sdi.getBackGroundColor(), sdi.isOriginalAspectRatioMaintained());
-		HPlayer *right = new HVIPlayer(iRight, NULL, baseDir, sdi.getDisplayType() == HDisplayType::HDisplayTypeFullScreen, sdi.getBackGroundColor(), sdi.isOriginalAspectRatioMaintained());
-		pmm->addPlayer(HPlayerPositionType::Left, left, iLeft);
-		pmm->addPlayer(HPlayerPositionType::Right, right, iRight);
-	}
-	
-	return pmm;
+	return createMediaManager(es.getStimulusDisplayInfo(), screenWidth, screenHeight);
 }
-
-
-HPreviewMediaManager* createPreviewMediaManager(const Habit::StimulusDisplayInfo& sdi)
-{
-	HPreviewMediaManager* pmm = new HPreviewMediaManager(sdi.getStimulusLayoutType());
-	HPlayer *psound = NULL;
-	if (sdi.getUseISS())
-	{
-		psound = new HAudioPlayer(-1, NULL, dirStimRoot);
-		pmm->addPlayer(HPlayerPositionType::Sound, p, -1);
-	}
-
-	HPlayer *p0 = new HVIPlayer(-1, NULL, dirStimRoot, sdi.getDisplayType() == HDisplayType::HDisplayTypeFullScreen, sdi.getBackGroundColor());
-	p0->setPreferBufferedStimulus(false);
-	HPlayer *p1l = new HVIPlayer(-1, NULL, dirStimRoot, sdi.getDisplayType() == HDisplayType::HDisplayTypeFullScreen, sdi.getBackGroundColor());
-	p1l->setPreferBufferedStimulus(false);
-	HPlayer *p1r = new HVIPlayer(-1, NULL, dirStimRoot, sdi.getDisplayType() == HDisplayType::HDisplayTypeFullScreen, sdi.getBackGroundColor());
-	p1r->setPreferBufferedStimulus(false);
-
-	pmm->addPlayers(p0, p1l, p1r, psound);
-
-
-}
-
-#endif  /* QT_VERSION < 5 */
