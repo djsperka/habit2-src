a1e32dc2a8d82af176458ea98548ca77e69f2fbe
Daniel Sperka <djsperka@ucdavis.edu>
2019-09-12 16:07:49 -0700

    Add new class PODs for sngle-pipeline stuff

--- apps/gstsp/MM1.h
+++ apps/gstsp/MM1.h
@@ -11,6 +11,43 @@
 #include <string>
 #include <gst/gst.h>
 #include <vector>
+#include <list>
+#include <map>
+
+
+enum class MMSrcType
+{
+	NONE,
+	VIDEO,
+	AUDIO
+};
+
+// MMStream corresponds to a single stream (audio or video) for a single source (e.g. a file).
+struct MMStream
+{
+	bool isVideo;	// if false, its audio
+	GstPad *srcpad;
+	gulong probeid;
+};
+
+// MMSource represents a single source (e.g. a file), which may produce one or more streams, but
+// no more than one each of audio, video.
+struct MMSource
+{
+	int counter;	// used to track async actions on all streams
+	bool prerolled;
+	GstElement *ele;	// a bin, but here probably uridecodebin
+	std::list<MMStream>;
+};
+
+// MMStim would correspond to StimulusSettings - the collection of individual sources (files)
+// that comprise the "stimulus" to be presented. The MMSrcType enum corresponds to the
+// PlayerPositionType, which distinguishes the various sources from one another.
+class MMStim
+{
+	std::map<MMSrcType, MMSource *>;
+};
+
 
 class MM1
 {
