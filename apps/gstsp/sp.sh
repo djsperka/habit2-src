8c84beb46000c7b39d41c6a46d6531314f9b6b3d
Daniel Sperka <djsperka@ucdavis.edu>
2019-09-30 16:33:15 -0700

    Made the swap happen in a bus callback. The msg is posted in the idle callback. Required playing with probe flags: GST_PAD_PROBE_TYPE_BLOCK_DOWNSTREAM|GST_PAD_PROBE_TYPE_IDLE seems to work. Must test without looping stim....

---
+++ apps/gstsp/sp.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+GST_DEBUG=3 \
+GST_DEBUG_DUMP_DOT_DIR=./ \
+./release/gstsp.app/Contents/MacOS/gstsp \
+/Users/dan/Desktop/BR.mp4 \
+/Users/dan/git/habit2-src/distribution/stim/examples/mov/Loop.mov \
+/Users/dan/git/habit2-src/distribution/stim/examples/images/plants/ginseng.jpg
+
