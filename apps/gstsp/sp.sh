118a2a158dc3b287147beaa1b0f66514a23ab06a
Daniel Sperka <djsperka@ucdavis.edu>
2020-05-28 12:56:56 -0700

    wip

--- apps/gstsp/sp.sh
+++ apps/gstsp/sp.sh
@@ -1,9 +1,8 @@
 #!/bin/sh
 
+GST_PLUGIN_PATH=/Users/dan/git/habit2-src/libs/gstqt/release \
 GST_DEBUG=3 \
 GST_DEBUG_DUMP_DOT_DIR=./ \
-./release/gstsp.app/Contents/MacOS/gstsp \
-/Users/dan/Desktop/BR.mp4 \
-/Users/dan/git/habit2-src/distribution/stim/examples/mov/Loop.mov \
-/Users/dan/git/habit2-src/distribution/stim/examples/images/plants/ginseng.jpg
+./release/gstsp.app/Contents/MacOS/gstsp -N \
+-N -s 127.0.0.1 -p 5255 -l /Users/dan/git/habit2-src/apps/gstsp/first.json -r /Users/dan/git/habit2-src/distribution/stim/examples
 
